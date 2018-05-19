9a34026acc54459f5283a5b2117bb60f8c5c478e
JohnENoonan <noonaj2@rpi.edu>
2018-05-19 17:00:57 -0400

    inital commit with working vuforia and start page

---
+++ MARQ_Game/Assets/Scripts/GameControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This class serves as the games state saving device. It is a Singleton Object that must be present
+ * in every scene
+*/
+public class GameControl : MonoBehaviour {
+
+    public static GameControl control; // singleton
+    public int team;
+
+	void Awake () {
+		if (control == null)
+        { // if this instance is the first
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        { // if object is not the one destroy it
+            Destroy(gameObject);
+        }
+	}
+	
+}
---
+++ MARQ_Game/Assets/Scripts/OpeningMenuControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OpeningMenuControls : MonoBehaviour {
+
+    // call on play btn
+	public void playGame()
+    {
+        GameControl.control.team = GameObject.Find("Dropdown").GetComponent<Dropdown>().value;
+        Debug.Log("set team to: " + GameControl.control.team);
+        // load next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
