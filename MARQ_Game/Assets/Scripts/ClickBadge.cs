300df4a0d4e227766d70a04909b34592c8d8f7ba
JohnENoonan <noonaj2@rpi.edu>
2018-07-04 18:42:29 -0400

    added empty place holder for clicking on badges

---
+++ MARQ_Game/Assets/Scripts/ClickBadge.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickBadge : MonoBehaviour {
+
+	void OnMouseDown()
+    {
+        Debug.Log("Clicked " + gameObject.name);
+    }
+}
