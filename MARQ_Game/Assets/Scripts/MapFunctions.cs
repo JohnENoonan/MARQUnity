ac4054297a4d98a4e1b56508d03992e2ae491088
JohnENoonan <noonaj2@rpi.edu>
2018-07-09 22:07:51 -0400

    added content for menu and finished map

--- MARQ_Game/Assets/Scripts/MapFunctions.cs
+++ MARQ_Game/Assets/Scripts/MapFunctions.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 // this class is attached to the map buttons that select which floor to look at
 public class MapFunctions : MonoBehaviour {
 
-    GameObject floorImg, backbtn;
+    GameObject floorImg;
     string floor;
 
     void Awake()
@@ -13,8 +13,6 @@ public class MapFunctions : MonoBehaviour {
         floor = gameObject.name;
         GameObject panel = gameObject.transform.parent.parent.gameObject;
         Debug.Assert(panel.name == "map content");
-        backbtn = panel.transform.GetChild(1).gameObject;
-        Debug.Assert(backbtn.name == "floor back btn");
         floorImg = panel.transform.GetChild(2).gameObject;
         Debug.Assert(floorImg.name == "individual floor");
     }
@@ -24,7 +22,6 @@ public class MapFunctions : MonoBehaviour {
     {
         // toggle elements
         floorImg.SetActive(true);
-        backbtn.SetActive(true);
         gameObject.transform.parent.gameObject.SetActive(false); // turn off cross section and buttons
         Sprite floorSprite = Resources.Load<Sprite>(floor);
         // assign the floors sprite
